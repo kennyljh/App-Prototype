20f313803a469ac485c9a5008f14045c6f995c13
Kenny Leong <kennyljh23@gmail.com>
2024-12-15 17:03:56 -0600

    added all account information retrieval for types USER and COMPANY

--- Backend/Career Connect/src/main/java/com/careerconnect/app/Accounts/AccountRepository.java
+++ Backend/Career Connect/src/main/java/com/careerconnect/app/Accounts/AccountRepository.java
@@ -2,7 +2,11 @@ package com.careerconnect.app.Accounts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
     Account findByUsernameId(Long username_id);
+
+    List<Account> findByAccountType(AccountType accountType);
 }
--- Backend/Career Connect/src/main/java/com/careerconnect/app/Accounts/CreateAccountDTO.java
+++ Backend/Career Connect/src/main/java/com/careerconnect/app/Accounts/CreateAccountDTO.java
@@ -2,6 +2,22 @@ package com.careerconnect.app.Accounts;
 
 /**
  * Data Transfer Object for account information and username
+ *
+ * Note: Currently the process of creating a new account requires
+ * the Client to send both Account and Username information.
+ *
+ * Issue: We can't have the parameter of the PostMapping
+ * for account creation to expect an Account type, Username
+ * type or both.
+ *
+ * Resolution: We create a Data Transfer Object (DTO) to contain
+ * information on both Account and Username, and retrieve them
+ * accordingly when needed.
+ * The Jackson library is used alongside with the @RequestBody
+ * annotation to deserialize the JSON payload into the
+ * CreateAccountDTO object. Automatically creating an empty
+ * instance of CreateAccountDTO, calling getters and setters
+ * to populate the fields based on the JSON payload.
  */
 public class CreateAccountDTO {
 
