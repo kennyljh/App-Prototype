5e65d386866fc2ef4518c8031e3fc64988ebe0a5
Kenny Leong <kennyljh23@gmail.com>
2024-12-15 14:26:54 -0600

    added basic account type to username creation server

---
+++ Backend/Career Connect/src/main/java/com/careerconnect/app/Usernames/UsernameRepository.java
@@ -0,0 +1,7 @@
+package com.careerconnect.app.Usernames;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsernameRepository extends JpaRepository<Username, Long> {
+
+}
