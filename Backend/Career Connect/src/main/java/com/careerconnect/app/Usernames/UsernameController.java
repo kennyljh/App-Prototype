df4d8eac638f70bc6ba1472603e9a34d9dc15900
Kenny Leong <kennyljh23@gmail.com>
2024-12-15 16:16:46 -0600

    added account info retrieval by username. Added username availability checking

--- Backend/Career Connect/src/main/java/com/careerconnect/app/Usernames/UsernameController.java
+++ Backend/Career Connect/src/main/java/com/careerconnect/app/Usernames/UsernameController.java
@@ -1,4 +1,23 @@
 package com.careerconnect.app.Usernames;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/username")
 public class UsernameController {
+
+    @Autowired
+    UsernameRepository usernameRepository;
+
+    @GetMapping("/check/{username}")
+    Boolean checkUsernameAvailability(@PathVariable String username){
+
+        Username desiredUsername = usernameRepository.findByUsername(username);
+        return desiredUsername == null;
+    }
+
 }
