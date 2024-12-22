95d5d8cad1207655da30978c153582c3cda38a1b
kennyljh <kennyljh23@gmail.com>
2024-12-22 16:54:56 -0600

    added career cluster vector assets

--- Frontend/app/src/main/java/com/example/careerconnect/LoginSignupElements/CareerClusterSelectionActivity.java
+++ Frontend/app/src/main/java/com/example/careerconnect/LoginSignupElements/CareerClusterSelectionActivity.java
@@ -4,16 +4,21 @@ import android.os.Bundle;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.careerconnect.R;
 
-public class CareerClusterSelection extends AppCompatActivity {
+public class CareerClusterSelectionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_career_cluster_selection);
+
+
+
+
 
         Button infoButton = findViewById(R.id.career_cluster_info_btn);
 
@@ -22,5 +27,14 @@ public class CareerClusterSelection extends AppCompatActivity {
             CareerClusterInfoDialogFragment dialog = new CareerClusterInfoDialogFragment();
             dialog.show(getSupportFragmentManager(), "Information");
         });
+    }
+
+    private class CareerClustersViewHolder extends RecyclerView.ViewHolder {
+
+
+
+
+
+
     }
 }
