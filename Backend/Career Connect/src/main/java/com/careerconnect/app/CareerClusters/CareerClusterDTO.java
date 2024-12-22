f3df833e910188456210416d9371e5b489f62616
Kenny Leong <kennyljh23@gmail.com>
2024-12-21 19:24:12 -0600

    @Valid javax annotation experimentation

--- Backend/Career Connect/src/main/java/com/careerconnect/app/CareerClusters/CareerClusterDTO.java
+++ Backend/Career Connect/src/main/java/com/careerconnect/app/CareerClusters/CareerClusterDTO.java
@@ -1,9 +1,15 @@
 package com.careerconnect.app.CareerClusters;
 
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 import java.util.Set;
 
 public class CareerClusterDTO {
 
+    @NotNull(message = "Selected career clusters cannot be null")
+    @Size(min = 1, message = "At least one career cluster must be selected")
     private Set<CareerCluster> selectedCareerClusters;
 
     public Set<CareerCluster> getSelectedCareerClusters() {
