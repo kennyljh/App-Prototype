fff8d5ec1f786b6ca53b1110bc01cda95582245a
kennyljh <kennyljh23@gmail.com>
2024-12-25 21:01:15 -0600

    xml layouts for profile customization exiting prototyping phase

--- Backend/Career Connect/src/main/java/com/careerconnect/app/CollegeMajors/CollegeMajorController.java
+++ Backend/Career Connect/src/main/java/com/careerconnect/app/CollegeMajors/CollegeMajorController.java
@@ -2,6 +2,8 @@ package com.careerconnect.app.CollegeMajors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,13 +19,23 @@ public class CollegeMajorController {
     CollegeMajorRepository collegeMajorRepository;
 
     @GetMapping("/get")
-    List<CollegeMajor> getAllMajors(){
-        return collegeMajorRepository.findAll(Sort.by(Sort.Order.asc("name")));
+    ResponseEntity<?> getAllMajors(){
+
+        if (collegeMajorRepository.findAll().isEmpty()){
+            String errorMsg = "No college majors found in repository";
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMsg);
+        }
+        return ResponseEntity.ok(collegeMajorRepository.findAll(Sort.by(Sort.Order.asc("name"))));
     }
 
     @GetMapping("/get/{prefix}")
-    List<CollegeMajor> getMajorsByPrefix(@PathVariable String prefix){
+    ResponseEntity<?> getMajorsByPrefix(@PathVariable String prefix){
+
+        if (collegeMajorRepository.findAll().isEmpty()){
+            String errorMsg = "No college majors found in repository";
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMsg);
+        }
         Sort sort = Sort.by(Sort.Order.asc("name"));
-        return collegeMajorRepository.findByNameContaining(prefix, sort);
+        return ResponseEntity.ok(collegeMajorRepository.findByNameContaining(prefix, sort));
     }
 }
